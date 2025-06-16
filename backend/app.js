6e7db7dba96cff59ffd79cd1098f624b8b7d3428
Luciano Lopez <lucianolop88@gmail.com>
2025-06-15 21:33:05 -0300

    Agregado el backend para la autenticacion

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/todos", require("./routes/todoRoutes"));
+
+module.exports = app;
