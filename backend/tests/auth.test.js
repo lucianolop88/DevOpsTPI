b928b099214b32fc6a38895f7f5d0eb4bcd4be42
Luciano Lopez <lucianolop88@gmail.com>
2025-06-16 00:27:30 -0300

    Agregado los test del backend

---
+++ backend/tests/auth.test.js
@@ -0,0 +1,36 @@
+const request = require("supertest");
+const app = require("../app"); // Asegurate que exportás `app` en lugar de hacer app.listen directamente
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+describe("Auth API", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://127.0.0.1:27017/exampleDatabase", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterAll(async () => {
+    const lastUser = await User.findOne().sort({ createdAt: -1 });
+    await User.findByIdAndDelete(lastUser._id);
+    await mongoose.connection.close();
+  });
+  test("registra un nuevo usuario", async () => {
+    const res = await request(app).post("/api/auth/register").send({
+      email: "test@user.com",
+      password: "123456",
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+  });
+
+  test("loguea al usuario registrado", async () => {
+    const res = await request(app).post("/api/auth/login").send({
+      email: "test@user.com",
+      password: "123456",
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+  });
+});
